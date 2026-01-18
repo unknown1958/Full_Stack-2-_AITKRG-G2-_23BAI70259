f9992f29186bc12ba0bf318023d20ce5cc784835
S S Sneha <152056505+unknown1958@users.noreply.github.com>
2026-01-18 23:33:24 +0530

    Add files via upload

---
+++ EXPERIMENT 1/ecotrack/src/Data/logs.js
@@ -0,0 +1,9 @@
+export const logs = [
+ { id: 1, activity: "Car Travel", carbon: 4 },
+ { id: 2, activity: "Electricity Usage", carbon: 6 },
+ { id: 3, activity: "Cycling", carbon: 0 },
+ { id: 4, activity: "Bus Travel", carbon: 3 },
+ { id: 5, activity: "Solar Energy Usage", carbon: 1 },
+ { id: 6, activity: "Flight Travel", carbon: 8 },
+];
+export default logs;
